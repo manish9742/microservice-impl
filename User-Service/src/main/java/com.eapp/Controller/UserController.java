2262284969533c34a053095b7a89f00f6261766d
Manish yadav <33183461+manish9742@users.noreply.github.com>
2025-05-13 16:27:53 +0530

    fallback method in microservice

--- User-Service/src/main/java/com.eapp/Controller/UserController.java
+++ User-Service/src/main/java/com.eapp/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.eapp.Controller;
 
 import com.eapp.entity.Users;
 import com.eapp.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,12 +26,26 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(users1);
 
     }
-
+    int retryCount=1;
     @GetMapping("/{users}")
+   // @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallBack")
+    @Retry(name="ratingHotelService",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<Users> createUser(@PathVariable String users){
         Users users1= userService.getUserById(users);
         return ResponseEntity.status(HttpStatus.OK).body(users1);
 
+    }
+
+    //remember the parameter name should be same as pathvariable in above method
+
+   public  ResponseEntity<Users> ratingHotelFallback(String users,Exception ex){
+       System.out.println(retryCount++);
+        Users  users1= Users.builder().email("abcd@gmail.com")
+                .name("hare rama")
+                .about("Service is down and its dummy response")
+                .userId("1234")
+                .build();
+       return ResponseEntity.ok(users1);
     }
 
      @GetMapping
